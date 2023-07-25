7ef63b35f0baca5516802391f725b68aa7bdb4e9
Yehonatan Shechter <ydshechter@gmail.com>
2023-07-25 17:40:05 +0300

    Build the color switch

---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,53 @@
+import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+import React from "react";
+import { motion } from "framer-motion";
+
+const ColorModeSwitch = () => {
+	const { toggleColorMode, colorMode } = useColorMode();
+
+	// Animation variants for the Switch (marginLeft animation)
+	const switchVariants = {
+		unchecked: { marginLeft: "0px" },
+		checked: { marginLeft: "10px" },
+	};
+
+	return (
+		<HStack align="center">
+			<motion.div
+				// animate={colorMode === "dark" ? "checked" : "unchecked"}
+				// variants={switchVariants}
+				transition={{ type: "spring", stiffness: 500, damping: 50 }}
+				style={{
+					borderRadius: "20px",
+					width: "40px",
+					height: "20px",
+					cursor: "pointer",
+					marginRight: "5px",
+
+					position: "relative",
+
+					backgroundColor: colorMode === "dark" ? "#48BB78" : "#CBD5E0",
+				}}
+				onClick={toggleColorMode}
+			>
+				<motion.div
+					animate={colorMode === "dark" ? "checked" : "unchecked"}
+					variants={switchVariants}
+					transition={{ type: "spring", stiffness: 300, damping: 15 }}
+					style={{
+						width: "15px",
+						height: "15px",
+						borderRadius: "50%",
+						backgroundColor: "#FFF",
+						position: "absolute",
+						top: "2px",
+						left: colorMode === "dark" ? "11.5px" : "2px",
+					}}
+				/>
+			</motion.div>
+			<Text>Dark Mode</Text>
+		</HStack>
+	);
+};
+
+export default ColorModeSwitch;
