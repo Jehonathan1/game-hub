5797453b2cf521170bce979c0d825c58d2afb7d0
Yehonatan Shechter <ydshechter@gmail.com>
2023-09-19 12:45:17 +0300

    Added server requests

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+const baseURL = process.env.NODE_ENV === 'development' ? '/api/rawg-proxy' : 'https://api.rawg.io/api';
+
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
+    baseURL,
     params: {
         key: import.meta.env.VITE_GAMES_API_KEY
     }
-})
+});
