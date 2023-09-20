d6408d564c9b3caa46485fd2bcdd3a32a1e49d43
Yehonatan Shechter <ydshechter@gmail.com>
2023-09-20 12:19:01 +0300

    Created a proxy server

---
+++ api/games.ts
@@ -0,0 +1,15 @@
+// api/games.js
+import axios from 'axios';
+
+module.exports = async (req, res) => {
+  try {
+    const response = await axios.get('https://api.rawg.io/api', {
+      params: {...req.query,
+      key: process.env.VITE_GAMES_API_KEY
+    }
+    });
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key: import.meta.env.VITE_GAMES_API_KEY
-    }
+    baseURL: '/api',
 });
