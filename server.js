b2cb839129b11a427466c9b2a23777b05b56e5cd
Yehonatan Shechter <ydshechter@gmail.com>
2023-09-21 16:24:00 +0300

    fixed the server endpoints

--- server.js
+++ server.js
@@ -8,13 +8,14 @@ dotenv.config(); // Load environment variables from .env file
 
 const app = express();
 const port = process.env.VITE_PORT || 3000; // You can choose another port if this one is in use
-// const port = 3000; // You can choose another port if this one is in use
 
 app.use(cors());
 
-app.get('/api/:endpoint', async (req, res) => {
+app.get('/api/:endpoint*', async (req, res) => {
   try {
-    const endpoint = req.params.endpoint;
+    const endpoint = req.params.endpoint + (req.params[0] || '');
+    console.log('Requested Endpoint:', endpoint); // Log the endpoint to the console
+    
     const response = await axios.get(`https://api.rawg.io/api/${endpoint}`, {
       params: { ...req.query, key: process.env.VITE_GAMES_API_KEY },
     });
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -2,13 +2,11 @@
 import axios from "axios";
 
 const port = import.meta.env.VITE_PORT || 3000;
-// src/services/api-client.ts
+
 console.log('API Client Port:', port); // Log the port to the console
 
-
 export default axios.create({
-    // DEVbaseURL: `http://localhost:${port}/api`, // Pointing to the new Express server
-    baseURL: `https://game-hub-1puq.onrender.com/api`, // Pointing to the new Express server
+    baseURL: `http://localhost:${port}/api`, // Pointing to the new Express server
 
     params: {
               key: import.meta.env.VITE_GAMES_API_KEY
