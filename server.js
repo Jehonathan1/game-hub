55933cdaa501e2d4c029470f4ef8e144d0c9d3f7
Yehonatan Shechter <ydshechter@gmail.com>
2023-09-21 13:53:59 +0300

    Created a proxy server

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import axios from 'axios';
+import cors from 'cors';
+
+const app = express();
+const port = process.env.VITE_PORT; // You can choose another port if this one is in use
+// const port = 3000; // You can choose another port if this one is in use
+
+app.use(cors());
+
+app.get('/api/:endpoint', async (req, res) => {
+  try {
+    const endpoint = req.params.endpoint;
+    const response = await axios.get(`https://api.rawg.io/api/${endpoint}`, {
+      params: { ...req.query, key: import.meta.env.VITE_GAMES_API_KEY },
+    });
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: `${error.message}` });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,12 @@
+
 import axios from "axios";
 
+const port = import.meta.env.VITE_PORT;
+
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
+    baseURL: `http://localhost:${port}/api`, // Pointing to the new Express server
+    // baseURL: `http://localhost:3000/api`, // Pointing to the new Express server
     params: {
-        key: import.meta.env.VITE_GAMES_API_KEY
-      }
+              key: import.meta.env.VITE_GAMES_API_KEY
+            }
 });
