24286835ff314bc0b36e5c750a45d7c44dce938d
Yehonatan Shechter <ydshechter@gmail.com>
2023-09-21 15:26:08 +0300

    updated baseURL

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -7,8 +7,9 @@ console.log('API Client Port:', port); // Log the port to the console
 
 
 export default axios.create({
-    baseURL: `http://localhost:${port}/api`, // Pointing to the new Express server
-    // baseURL: `http://localhost:3000/api`, // Pointing to the new Express server
+    // DEVbaseURL: `http://localhost:${port}/api`, // Pointing to the new Express server
+    baseURL: `https://game-hub-1puq.onrender.com/api`, // Pointing to the new Express server
+
     params: {
               key: import.meta.env.VITE_GAMES_API_KEY
             }
